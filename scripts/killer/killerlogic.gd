b9187a79bdd3a11be2788a38f8b7d0793fd52f87
chrisguayara <chrisg.deu@gmail.com>
2025-09-28 22:28:10 -0400

    killerstatemachine
    state machine setup , pass delta

--- scripts/killer/killerlogic.gd
+++ scripts/killer/killerlogic.gd
@@ -1,6 +1,13 @@
 extends CharacterBody3D
 
-var states: Array = ["stalking", "hunting", "hurt", "entry"]
+var states: Dictionary = {
+	"stalking": _stalking_state,
+	"hunting": _hunting_state,
+	"hurt": _hurt_state,
+	"entry": _entry_state
+}
+
+var currentstate: String = "stalking"
 
 var health = 200
 var visitedEntries = {}
@@ -8,9 +15,29 @@ var visitedEntries = {}
 var speed = 10
 var isRunning = false
 var isDead = false
-
-func _ready():
-	pass
+var charactername: String = "breach"
 
 func _physics_process(delta):
+	
+	states[currentstate].call(delta)
+	
+
+
+
+
+
+#state logic !
+
+func _stalking_state(delta):
 	pass
+
+func _hunting_state(delta):
+	pass
+	
+
+func _hurt_state(delta):
+	pass
+
+func _entry_state(delta):
+	pass
+	
