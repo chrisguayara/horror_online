7c610cbdfa232c0c6b6710d1a5bb247ca88e6ccd
chrisguayara <chrisg.deu@gmail.com>
2025-10-19 01:51:45 -0400

    playerInventory&Remington

--- scenes/inventoryinhand.gd
+++ scenes/inventoryinhand.gd
@@ -1,3 +1,16 @@
 extends Marker3D
 
 var inInventory = false
+var inventory = {}
+@export var remington : Node3D
+
+
+func _on_inventory_active_in_hand(firstItem:String ):
+	#check dictionary for dict[firstItem]
+	var curr = ""
+	for item in inventory:
+		item.hide
+	inventory[firstItem] = show()
+	
+	##fix code this is just pseudo
+	
--- scripts/player/inventory.gd
+++ scripts/player/inventory.gd
@@ -4,8 +4,19 @@ extends Node3D
 var inventory_space = 5
 var itemAmount = 0
 signal addSound
+signal activeInHand
+@onready var hand = $"../head/camera/hand"
+
+func checkActive():
+	if main_inventory:
+		var activeHandItem = main_inventory[0]
+		emit_signal("activeInHand",main_inventory[0])
+		#this makes it so that the first item in inventory will always be the in hand item. 
+
 
 func add_to_inventory(item: Dictionary):
+	
+		
 	if itemAmount < inventory_space:
 		main_inventory.append(item)
 		emit_signal("addSound", "inventory")
