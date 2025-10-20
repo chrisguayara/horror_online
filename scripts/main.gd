3fdb904224ca238e865c653758fbb8f4208c98ec
chrisguayara <chrisg.deu@gmail.com>
2025-10-20 17:26:28 -0400

    fullscreen

--- scripts/main.gd
+++ scripts/main.gd
@@ -4,3 +4,12 @@ extends Node3D
 func _input(event):
 	if event.is_action_pressed("quit"):
 		get_tree().quit()
+	if event.is_action_pressed("fullscreen"):
+		toggle_fullscreen()
+
+func toggle_fullscreen():
+	var mode:= DisplayServer.window_get_mode()
+	if mode == DisplayServer.WindowMode.WINDOW_MODE_FULLSCREEN:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	if mode == DisplayServer.WindowMode.WINDOW_MODE_WINDOWED:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
