f530872a0caf8f7f7f2f4a3ee76b62757679a4f4
Christopher Guayara <chrisg.deu@gmail.com>
2026-06-17 00:02:03 -0400

    tv_materials

--- scripts/interactRay.gd
+++ scripts/interactRay.gd
@@ -1,21 +1,23 @@
 extends RayCast3D
 
-
 @onready var prompt = $Prompt
-
 
 func _ready():
 	enabled = true
+	prompt.visible = false
 
 func _process(delta):
-	#
-	#prompt.text = ""
 	if is_colliding():
 		var collider = get_collider()
-		
-		if collider is Interactable:
-			
-			prompt.text = collider.prompt_msg
-			if Input.is_action_just_pressed("interact"):
 
-				collider.prompt_msg = ""
+		if collider is Interactable:
+			prompt.visible = true
+			prompt.text = collider.prompt_msg
+
+			if Input.is_action_just_pressed("interact"):
+				collider.interact(get_parent().get_parent().get_parent())  # or however you pass the player
+		else:
+			prompt.visible = false  # colliding, but not an Interactable
+	else:
+		prompt.visible = false    # not colliding at all
+		prompt.text = ""
